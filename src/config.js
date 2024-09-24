cd8b649200933d085433fd49be29fba1b165a062
WhitneySt <whitneystena418@gmail.com>
2024-09-24 15:44:17 -0500

    Integrando la api de instagram

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const INSTAGRAM_APP_ID = "907662524513353";
 export const INSTAGRAM_SECRET_KEY = "";
-export const REDIRECT_URL = "https://whitneyst.github.io/prueba_concepto_APIF-I";
+export const REDIRECT_URL = "https://whitneyst.github.io/prueba_concepto_APIF-I/";
 export const URL_API = "http://localhost:3000/";
 
 const endpoints = {
